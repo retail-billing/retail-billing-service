b9dd308e5f8a16ee6f00f75ef54433d6ddff53a0
Ashish Gupta <ashishg0926@gmail.com>
2025-05-18 22:30:49 +0530

    initial setup

---
+++ src/main/java/com/ashish/retailbillingservice/RetailBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.retailbillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailBillingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ashish/retailbillingservice/RetailBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.retailbillingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailBillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
